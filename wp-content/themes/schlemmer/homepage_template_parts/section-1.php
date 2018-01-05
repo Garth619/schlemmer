a5da9d54ff0fa6b3d281b14e4175460c240724d5
Garrett Cullen <Garth619@users.noreply.github.com>
2018-01-05 09:53:59 -0800

    push

--- wp-content/themes/schlemmer/homepage_template_parts/section-1.php
+++ wp-content/themes/schlemmer/homepage_template_parts/section-1.php
@@ -13,6 +13,8 @@
 		
 		<span class="tagline">Fighting for what matters to you</span><!-- tagline -->
 		
+		<span class="get_started_button">Lets Get Started </span><!-- get_started_button -->
+		
 		
 	</div><!-- section_one_content -->
 		
@@ -20,7 +22,34 @@
 	
 	<div class="section_right">
 		
+		<div class="menu_wrapper">
+			
+			<div class="phone_wrapper">
+				
+				<a href="tel:(888) 458-8500">(888) 458-8500</a>
+				
+			</div><!-- phone_wrapper -->
+			
+			<div class="menu_prompt">
+				
+				<span class="menu_bars"></span><!-- menu_bars -->
+				
+				<span class="menu_bars"></span><!-- menu_bars -->
+				
+				<span class="menu_bars"></span><!-- menu_bars -->
+				
+				<span class="menu_title">MENU</span><!-- menu_title -->
+				
+			</div><!-- menu_prompt -->
+			
+		</div><!-- menu_wrapper -->
 		
+		<div class="selling_points">
+			
+			
+			
+			
+		</div><!-- selling_points -->
 		
 	</div><!-- section_right -->
 	
