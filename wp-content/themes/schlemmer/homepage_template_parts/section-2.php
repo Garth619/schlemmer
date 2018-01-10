73ccc3aa0fb52dcf608cf2639daaec314f677c0c
Garrett Cullen <Garth619@users.noreply.github.com>
2018-01-10 11:04:05 -0800

    push

--- wp-content/themes/schlemmer/homepage_template_parts/section-2.php
+++ wp-content/themes/schlemmer/homepage_template_parts/section-2.php
@@ -8,12 +8,30 @@
 			<h2 class="small_header">Personal Injury Attorneys</h2><!-- small_header -->
 		
 			<h1 class="large_header">Brooklyn</h1><!-- large_header -->
-		
+			
 		</div><!-- section_two_header -->
+			
+			<div class="section_two_content">
+				
+				 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed doiusmod tempor ncididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis                  nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+
+<h2>lorem ipsum dolor sit amet consecutor</h2>
+
+<p>Iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+
+<p>Iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi. nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+				
+			</div><!-- section_two_content -->
+			
+			<div class="section_two_img_wrapper">
+				
+				<img class="section_two_img" src="<?php bloginfo('template_directory');?>/images/content-taxi-img-desktop.jpg"/>
+				
+			</div><!-- section_two_img_wrapper -->
+		
+		
 	
 	
 	</div><!-- inner_section_two -->
 	
-	
-
 </section><!-- section_two -->
