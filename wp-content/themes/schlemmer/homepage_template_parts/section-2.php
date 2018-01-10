5498fef1ea39061346653e42f695ff0eb2501e99
Garrett Cullen <Garth619@users.noreply.github.com>
2018-01-09 16:18:48 -0800

    push

---
+++ wp-content/themes/schlemmer/homepage_template_parts/section-2.php
@@ -0,0 +1,19 @@
+<section id="section_two">
+	
+	<div class="inner_section_two">
+	
+	
+		<div class="section_two_header">
+		
+			<h2 class="small_header">Personal Injury Attorneys</h2><!-- small_header -->
+		
+			<h1 class="large_header">Brooklyn</h1><!-- large_header -->
+		
+		</div><!-- section_two_header -->
+	
+	
+	</div><!-- inner_section_two -->
+	
+	
+
+</section><!-- section_two -->
