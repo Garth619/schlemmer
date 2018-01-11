b2006c230de38467500ed038e634a645b400c923
Garrett Cullen <Garth619@users.noreply.github.com>
2018-01-11 13:30:57 -0800

    push

---
+++ wp-content/themes/schlemmer/homepage_template_parts/section-4.php
@@ -0,0 +1,23 @@
+<section id="section_four">
+	
+	<div class="sec_four_left">
+		
+		<div class="sec_four_content">
+			
+			<span class="large_header"><span>what</span> <span>our</span> <span>clients</span> <span>say</span></span><!-- large_header -->
+			
+			<a class="testimonial_button" href="">View All Testimonials</a>
+			
+		</div><!-- sec_four_content -->
+	
+	</div><!-- sec_four_left -->
+	
+	<div class="sec_four_right">
+		
+		
+	
+	</div><!-- sec_four_right -->
+	
+	
+	
+</section><!-- section_four -->
