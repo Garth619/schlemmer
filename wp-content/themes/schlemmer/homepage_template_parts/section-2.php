13ee7a6f417100d31b839a7ecda875380463139e
Garrett Cullen <Garth619@users.noreply.github.com>
2018-01-10 16:30:33 -0800

    push

--- wp-content/themes/schlemmer/homepage_template_parts/section-2.php
+++ wp-content/themes/schlemmer/homepage_template_parts/section-2.php
@@ -11,7 +11,7 @@
 			
 		</div><!-- section_two_header -->
 			
-			<div class="section_two_content">
+			<div class="section_two_content content">
 				
 				 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed doiusmod tempor ncididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis                  nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
 
@@ -19,13 +19,18 @@
 
 <p>Iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
 
-<p>Iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi. nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+<p>Iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. <span class="hide">Iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi. nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</span></p>
 				
 			</div><!-- section_two_content -->
 			
 			<div class="section_two_img_wrapper">
 				
+				
 				<img class="section_two_img" src="<?php bloginfo('template_directory');?>/images/content-taxi-img-desktop.jpg"/>
+				
+				
+				<img class="section_two_img_laptop" src="<?php bloginfo('template_directory');?>/images/content-taxi-img-laptop.jpg"/>
+				
 				
 			</div><!-- section_two_img_wrapper -->
 		
---
+++ wp-content/themes/schlemmer/homepage_template_parts/section-3.php
@@ -0,0 +1,42 @@
+<section id="section_three">
+	
+	<div class="section_three_inner">
+		
+		<?php echo file_get_contents("wp-content/themes/schlemmer/images/new_brooklyn.svg"); ?>
+		
+		<div class="sec_three_sidebar">
+			
+			<ul>
+				
+				<li><a href="">Lorem ipsum dolor sit</a></li>
+				<li><a href="">adc adc a dcasd c</a></li>
+				<li><a href="">adc adc a dcasd c</a></li>
+				<li><a href="">adc adc a dcasd c</a></li>
+				<li><a href="">adc adc a dcasd c</a></li>
+				<li><a href="">adc adc a dcasd c</a></li>
+				<li><a href="">adc adc a dcasd c</a></li>
+				<li><a href="">adc adc a dcasd c</a></li>
+				<li><a href="">adc adc a dcasd c</a></li>
+				<li><a href="">adc adc a dcasd c</a></li>
+				
+			</ul>
+			
+			<a class="pa_button" href="">View All Practice Areas</a><!-- pa_button -->
+			
+		</div><!-- sec_three_sidebar -->
+		
+		<div class="sec_three_content content">
+			
+			
+			<span class="large_header">lorem ipsum dolor sit amet consecutor lorem ipsum dolor sit amet consecutor</span>
+			
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irurorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequatuis aute iru. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irurorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequatuis aute iru.</p>
+
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irurorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequatuis aute iru.</p>
+			
+			
+		</div><!-- sec_three_content -->
+		
+	</div><!-- section_three_inner -->
+	
+</section><!-- section_three -->
