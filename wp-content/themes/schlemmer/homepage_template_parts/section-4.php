f7184739d6b10d0ac9d653f71f4e69e978267fc5
Garrett Cullen <Garth619@users.noreply.github.com>
2018-01-11 14:41:18 -0800

    push

--- wp-content/themes/schlemmer/homepage_template_parts/section-4.php
+++ wp-content/themes/schlemmer/homepage_template_parts/section-4.php
@@ -14,6 +14,71 @@
 	
 	<div class="sec_four_right">
 		
+		<div class="testimonial_title_wrapper">
+			
+			<?php echo file_get_contents("wp-content/themes/schlemmer/images/ny.svg"); ?>
+			
+			<span class="testimonial_title">Testimonials</span><!-- testi_title -->
+			
+		</div><!-- testi_title_wrapper -->
+		
+		
+		<div class="testimonial_slideshow">
+			
+			<div class="single_testimonial_slide">
+				
+				<span class="large_header">I am very grateful to Mr. Schlemmer</span><!-- large_header -->
+				
+				<span class="slide_content">I had been in a car accident and had hired an attorney that did nothing to help me. I had a chance meeting and met Mr. Schlemmer. He not only took over my case, he accompanied me to doctor appointments, and genuinely cared about me. The settlement was very fair and that was only because of Mr. Schlemmer's expertise. Anytime I had questions he answered my calls and addressed my concerns. My injury was devastating and I am very grateful to Mr. Schlemmer.</span><!-- slide_content -->
+				
+				<span class="blackline"></span><!-- blackline -->
+				
+				<span class="testimonial_name">PaTRICIA J.</span><!-- testimonial_name -->
+				
+			</div><!-- single_testimonial_slide -->
+			
+			
+			
+			<div class="single_testimonial_slide">
+				
+				<span class="large_header">Enim ad minim veniam, quis nost</span><!-- large_header -->
+				
+				<span class="slide_content">  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea</span><!-- slide_content -->
+				
+				<span class="blackline"></span><!-- blackline -->
+				
+				<span class="testimonial_name">Name</span><!-- testimonial_name -->
+				
+			</div><!-- single_testimonial_slide -->
+			
+			
+			<div class="single_testimonial_slide">
+				
+				<span class="large_header">Enim ad minim veniam, qui  veniam qui veniam quis nost</span><!-- large_header -->
+				
+				<span class="slide_content">  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud d minim veniam, quis nosd minim veniam, quis nosd minim veniam, quis nos exercitation ullamco laboris nisi ut aliquip ex ea</span><!-- slide_content -->
+				
+				<span class="blackline"></span><!-- blackline -->
+				
+				<span class="testimonial_name">Name</span><!-- testimonial_name -->
+				
+			</div><!-- single_testimonial_slide -->
+			
+			
+			
+			
+			
+			
+			
+		</div><!-- testimonial_slideshow -->
+		
+		<div class="next_back_wrapper">
+				
+				<div class="prev slide_button">Prev</div><!-- next -->
+				
+				<div class="next slide_button">Next</div><!-- next -->
+				
+			</div><!-- next_back_wrapper -->
 		
 	
 	</div><!-- sec_four_right -->
