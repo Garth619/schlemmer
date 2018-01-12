af8c95016537be7fbe282ebcd1ed222949e56622
Garrett Cullen <Garth619@users.noreply.github.com>
2018-01-11 16:24:34 -0800

    push

---
+++ wp-content/themes/schlemmer/homepage_template_parts/section-5.php
@@ -0,0 +1,15 @@
+<section id="section_five">
+	
+	
+	<div class="section_five_inner">
+		
+		
+		<span class="large_header">In the past 5<br/> years our firm has won over $20 million </span><!-- large_header -->
+		
+		
+	</div><!-- section_five_inner -->
+	
+	
+	
+	
+</section><!-- section_five -->
