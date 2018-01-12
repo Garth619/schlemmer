cfe607fd4fa5a721cdb42c9f2be99b78d5a27fb3
Garrett Cullen <Garth619@users.noreply.github.com>
2018-01-12 09:31:06 -0800

    push

--- wp-content/themes/schlemmer/homepage_template_parts/section-5.php
+++ wp-content/themes/schlemmer/homepage_template_parts/section-5.php
@@ -6,6 +6,41 @@
 		
 		<span class="large_header">In the past 5<br/> years our firm has won over $20 million </span><!-- large_header -->
 		
+		<div class="section_five_content">
+			
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officiab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explica ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explibo.</p>
+
+ 		</div><!-- section_five_content -->
+ 		
+ 		
+ 		<div class="case_results_wrapper">
+	 		
+	 		<div class="single_case_results">
+		 		
+		 		<span class="case_results_small_header">Verdict</span><!-- small_header -->
+		 		
+		 		<span class="case_results_large_header">Lorem VS Lorem</span><!-- small_header -->
+		 		
+	 		</div><!-- single_case_results -->
+	 		
+	 		<div class="single_case_results">
+		 		
+		 		<span class="case_results_small_header">Settlement</span><!-- small_header -->
+		 		
+		 		<span class="case_results_large_header">Lorem VS Lorem</span><!-- small_header -->
+		 		
+	 		</div><!-- single_case_results -->
+	 		
+	 		<div class="single_case_results">
+		 		
+		 		<span class="case_results_small_header">Verdict</span><!-- small_header -->
+		 		
+		 		<span class="case_results_large_header">Lorem VS Lorem</span><!-- small_header -->
+		 		
+	 		</div><!-- single_case_results -->
+	 		
+ 		</div><!-- case_results_wrapper -->
+		
 		
 	</div><!-- section_five_inner -->
 	
