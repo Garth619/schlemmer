528400109d34a778272387e72e72e7e7474a1ca8
Garrett Cullen <Garth619@users.noreply.github.com>
2018-01-12 16:04:10 -0800

    push

---
+++ wp-content/themes/schlemmer/homepage_template_parts/section-6.php
@@ -0,0 +1,34 @@
+<section id="section_six">
+	
+	
+	<div class="sec_six_att_wrapper">
+		
+		
+		
+	</div><!-- section_six_att_wrapper -->
+	
+	<div class="sec_six_content_wrapper">
+		
+		<div class="sec_six_title_wrapper">
+		
+			<span class="small_header">Attorney</span><!-- small_header -->
+		
+			<span class="large_header">Paul Schlemmer</span><!-- large_header -->
+			
+		</div><!-- sec_six_title_wrapper -->
+		
+		<div class="sec_six_content content">
+				
+				<p>Paul N. Schlemmer is board certified in civil trial advocacy by the National Board in Trial Advocacy. This distinction is unique, and held by only 4% of the lawyers in the U.S. This prestigious certification requires exceptional performance, as well as recommendations from peers and judges. In addition, Mr. Schlemmer is board certified in pre-trial advocacy by the National Board of Legal Specialty Certification.</p>
+
+				<p>Due to hard work and dedication, Mr. Schlemmer has obtained numerous large money judgments and settlements. However he believes that what is more important than the dollar amount of the win is the ability to secure justice for the members of our community.</p>
+				
+				<span class="att_name">Meet Paul</span><!-- att_name -->
+				
+			</div><!-- sec_six_content -->
+
+		
+	</div><!-- sec_six_content -->
+	
+	
+</section><!-- section_six -->
