dce0cbcfe4695ecc67e5e27a9ea41447975d8944
Garrett Cullen <Garth619@users.noreply.github.com>
2018-01-18 11:21:44 -0800

    push

--- wp-content/themes/schlemmer/page-about.php
+++ wp-content/themes/schlemmer/page-about.php
@@ -8,7 +8,52 @@
 
 <div class="container" style="min-height:800px">
 	
-		<h1><?php the_title();?></h1>
+		<div class="int_about">
+			
+			
+			<h1>About</h1>
+			
+			<span class="small_header">The Schlemmer Firm</span><!-- small_header -->
+			
+			
+			<div class="int_about_bottom">
+				
+				
+				<div class="int_about_content">
+					
+					
+					<div class="int_about_content_inner content">
+					
+						<h2>Committed to justice</h2>
+
+						<p>The Schlemmer Firm is committed to defending the rights of victims who have been injured through the negligence of others. Whether the injury involves a medical malpractice claim, trucking accident, or trip and fall, Mr. Schlemmer is focused on providing his clients quality legal representation they deserve.</p>
+
+						<h2>finding solutions</h2>
+
+						<p>If you or a loved one has been injured due to the negligence of another, The Schlemmer Firm is here to assist you in what is often an extremely difficult time. Mr. Schlemmer understands that injuries cause not only physical pain, but also emotional distress, financial problems, and the inability to carry on with daily activities.</p>
+
+						<p>You deserve to be compensated for your injuries. Depending on the circumstances of your case, you could be entitled to reimbursement for your medical expenses, pain and suffering as well as loss of income. Recovering from an injury can be a lonely, scary journey. The Schlemmer Firm is here to fight for your rights and provide you with solutions, so you can get on with living your life.</p>
+					
+					</div><!-- int_about_content_inner -->
+					
+					
+				</div><!-- int_about_content -->
+				
+				
+				
+				<div class="int_about_pic">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/int_about_img.jpg"/>
+					
+				</div><!-- int_about_pic -->
+				
+				
+				
+			</div><!-- int_about_bottom -->
+			
+			
+			
+		</div><!-- int_about -->
 	
 </div><!-- container -->
 
