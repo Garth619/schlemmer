5cf58cd95bc1a6f30cbba1fa14413cd0c48a2bf9
Garrett Cullen <Garth619@users.noreply.github.com>
2018-01-22 09:44:17 -0800

    push

--- wp-content/themes/schlemmer/page-about.php
+++ wp-content/themes/schlemmer/page-about.php
@@ -13,7 +13,7 @@
 			
 			<h1>About</h1>
 			
-			<span class="small_header">The Schlemmer Firm</span><!-- small_header -->
+			<span class="small_header"><?php the_field( 'sub_header' ); ?></span><!-- small_header -->
 			
 			
 			<div class="int_about_bottom">
@@ -24,15 +24,7 @@
 					
 					<div class="int_about_content_inner content">
 					
-						<h2>Committed to justice</h2>
-
-						<p>The Schlemmer Firm is committed to defending the rights of victims who have been injured through the negligence of others. Whether the injury involves a medical malpractice claim, trucking accident, or trip and fall, Mr. Schlemmer is focused on providing his clients quality legal representation they deserve.</p>
-
-						<h2>finding solutions</h2>
-
-						<p>If you or a loved one has been injured due to the negligence of another, The Schlemmer Firm is here to assist you in what is often an extremely difficult time. Mr. Schlemmer understands that injuries cause not only physical pain, but also emotional distress, financial problems, and the inability to carry on with daily activities.</p>
-
-						<p>You deserve to be compensated for your injuries. Depending on the circumstances of your case, you could be entitled to reimbursement for your medical expenses, pain and suffering as well as loss of income. Recovering from an injury can be a lonely, scary journey. The Schlemmer Firm is here to fight for your rights and provide you with solutions, so you can get on with living your life.</p>
+						<?php the_field( 'about_content_one' ); ?>
 					
 					</div><!-- int_about_content_inner -->
 					
@@ -43,46 +35,38 @@
 				
 				<div class="int_about_pic">
 					
-					<img src="<?php bloginfo('template_directory');?>/images/int_about_img.jpg"/>
+					<?php $about_image = get_field( 'about_image' ); ?>
+					
+					<img src="<?php echo $about_image['url']; ?>" alt="<?php echo $about_image['alt']; ?>" />
 					
 				</div><!-- int_about_pic -->
 				
 				
-				<div class="about_logos">
+				
+				<?php if(get_field('awards')): ?>
+				
+					<div class="about_logos">
+				 
+					<?php while(has_sub_field('awards')): ?>
+				 
+						
+						<div class="logo_slide">
+						
+							<?php $image = get_sub_field( 'image' ); ?>
+							
+							<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+						
+						</div><!-- logo_slide -->
+						
+				    
+					<?php endwhile; ?>
 					
-					
-					<div class="logo_slide">
-						
-						<img src="<?php bloginfo('template_directory');?>/images/award_1.png"/>
-						
-					</div><!-- logo_slide -->
-					
-					<div class="logo_slide">
-						
-						<img src="<?php bloginfo('template_directory');?>/images/award_2.png"/>
-						
-					</div><!-- logo_slide -->
-					
-					<div class="logo_slide">
-						
-						<img src="<?php bloginfo('template_directory');?>/images/award_3.png"/>
-						
-					</div><!-- logo_slide -->
-					
-					<div class="logo_slide">
-						
-						<img src="<?php bloginfo('template_directory');?>/images/award_4.png"/>
-						
-					</div><!-- logo_slide -->
-					
-					<div class="logo_slide">
-						
-						<img src="<?php bloginfo('template_directory');?>/images/award_5.png"/>
-						
-					</div><!-- logo_slide -->
-					
-					
-				</div><!-- about_logos -->
+					</div><!-- about_logos -->
+				 
+				<?php endif; ?>
+				
+				
+				
 				
 				
 				
@@ -101,10 +85,10 @@
 				<div class="second_int_about_content content">
 					
 					
-					<p class="about_intro">lorem ipsum dolor sit amet consecutor lorem ipsum dolor sit amet. lorem ipsum dolor sit amet consecutor lorem ipsum dolor sit amet consecutor lorem ipsum</p><!-- about_intro -->
+					<p class="about_intro"><?php the_field( 'about_content_two_intro' ); ?></p><!-- about_intro -->
 					
 					
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed doiusmod tempor ncididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nisi ut aliquip ex ea commodo nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. This is the link body copy style. This is the hover link body copy style. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+					<?php the_field( 'about_content_two' ); ?>
 					
 				</div><!-- second_int_about_content -->
 				
